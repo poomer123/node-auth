d927309d05837a584497cd4d800480057cd848af
zobry <apirat.chiwa@gmail.com>
2020-04-20 15:52:49 +0700

    test log cookie

--- src/routes/number.js
+++ src/routes/number.js
@@ -2,6 +2,8 @@ const router = require('express').Router()
 
 router.get('/', (req, res) => {
 	console.log(res.locals.userDetail)
+	res.cookie('hello', 'HELLO')
+	res.cookie('world', 'WORLD', { httpOnly: true })
 	res.json({})
 })
 
