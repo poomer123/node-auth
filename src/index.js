a5334a32081deccbccabae1a8d2c459629e1a3e2
zobry <apirat.chiwa@gmail.com>
2020-05-18 13:48:52 +0700

    comment code for testing

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = require('./express');
 const path = require('path');
 
-app.use('/upload', require('./routes/upload'));
+// app.use('/upload', require('./routes/upload'));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -19,10 +19,10 @@ app.get('/healthcheck', () => {
 	});
 });
 
-app.use('/auth', require('./routes/auth'));
-app.use('/number', require('./middlewares/jwt'), require('./routes/number'));
+// app.use('/auth', require('./routes/auth'));
+// app.use('/number', require('./middlewares/jwt'), require('./routes/number'));
 // app.use('/number', require('./routes/number'))
-app.use('/user', require('./middlewares/admin'), require('./routes/user'));
+// app.use('/user', require('./middlewares/admin'), require('./routes/user'));
 // app.use('/persons', require('./routes/person'))
 // app.use('/session', require('./routes/session'))
 // app.use('/cookie', require('./routes/cookie'))
